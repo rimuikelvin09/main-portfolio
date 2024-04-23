07c8b05934292140b70030536dfc19b17ec1e7f7
Kelvin Rimui <rimuikelvin4@gmail.com>
2024-04-23 15:42:19 +0300

    1st fix on responsiveness, removed the Overflow-x

--- pages/index.js
+++ pages/index.js
@@ -41,18 +41,18 @@ export default function Home() {
         {/**Header section */}
 
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">AlphaTech Insights</h1>
+          <nav className="py-4 md:py-6 lg:py-10 mb-4 md:mb-6 lg:mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-lg md:text-xl lg:text-2xl">AlphaTech Insights</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
-                  className=" cursor-pointer text-2xl"
+                  className="cursor-pointer text-lg md:text-xl"
                 />
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-slate-700 text-white px-4 py-2 border-none rounded-md ml-8"
+                  className="bg-gradient-to-r from-cyan-500 to-slate-700 text-white px-3 md:px-4 py-1 md:py-2 border-none rounded-md ml-4 md:ml-8"
                   href="https://www.maxino.dev/games"
                 >
                   Game
@@ -60,24 +60,25 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10 py-10 drop-shadow-md">
-            <h2 className="text-5xl py-2 text-slate-700 font-extrabold dark:text-white md:text-6xl">
+          <div className="text-center p-4 md:p-6 py-6 md:py-10 drop-shadow-md">
+            <h2 className="text-3xl md:text-5xl py-2 text-slate-700 font-extrabold dark:text-white">
               Kelvin
               <span className="text-sky-500">Rimui</span>
-
             </h2>
-            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+            <h3 className="text-xl md:text-2xl py-2 dark:text-white">
               IT Consultant.
             </h3>
-            <div className="mx-auto bg-gradient-to-b from-sky-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96 drop-shadow-md hover:drop-shadow-xl">
+            <div className="mx-auto bg-gradient-to-b from-sky-500 rounded-full w-48 md:w-80 h-48 md:h-80 relative overflow-hidden mt-10 md:mt-20 lg:mt-24">
               <Image src={profile} alt="" layout="fill" objectFit="cover" />
             </div>
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-3xl mx-auto md:text-xl">
+            <p className="text-md md:text-md py-3 md:py-5 leading-7 md:leading-8 text-gray-800 dark:text-gray-200 max-w-md md:max-w-3xl mx-auto">
               My team specializes in seamlessly integrating business operations with cutting-edge IT solutions.
               With a focus on websites, CRM systems, hardware implementation, and a diverse background spanning various industries,
               we are your trusted partner in optimizing your technological infrastructure for maximum efficiency and growth.
             </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+
+            {/**Social Icons */}
+            <div className="text-3xl md:text-5xl flex justify-center gap-8 md:gap-16 py-2 md:py-3 text-gray-600 dark:text-gray-400">
               <a href="https://twitter.com/rimui09">
                 <AiFillTwitterCircle />
               </a>
@@ -91,16 +92,15 @@ export default function Home() {
                 <AiFillGithub />
               </a>
             </div>
-
           </div>
         </section>
 
 
         {/** Services Section */}
-        <section className="px-20" >
+        <section className="px-4 md:px-6 lg:px-20" >
           <div >
             <h3 className="text-3xl py-1 text-slate-700 dark:text-white font-bold">Our Services</h3>
-            <p className="text-md py-2 leading-8  text-gray-800 dark:text-gray-200  mx-auto md:text-xl">
+            <p className="text-md py-2 leading-8  text-gray-800 dark:text-gray-200  mx-auto md:text-l">
               Since the beginning of our journey we've worked with
               <span className="text-sky-600"> Financial Institutions</span>,
               <span className="text-sky-600"> Government Agencies</span>,
@@ -110,62 +110,70 @@ export default function Home() {
               We've also collaborated with talanted people to create digital products
               for both business and consumer use.
             </p>
-
           </div>
-          <div className="lg:flex gap-10">
-            <div className="text-left shadow-xl hover:shadow-2xl p-10 rounded-xl my-10  dark:bg-slate-200 flex-1">
+
+          {/**Services Cards */}
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-10">
+            <div className="text-left leading-8 shadow-xl hover:shadow-2xl p-4 md:p-10 rounded-xl my-4 md:my-10 dark:bg-slate-200">
               <Image src={design} alt="" width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 text-slate-600 ">
+              <h3 className="text-lg md:text-xl font-medium pt-4 md:pt-8 pb-2 text-slate-600 ">
                 Web Development
               </h3>
-              <p className="py-2">
+              <p className="text-sm  md:text-base py-2 ">
                 Experience the seamless fusion of innovation and functionality with our
                 expertly crafted web solutions, where every line of code tells a story of mastery.
               </p>
-              <h4 className="py-4 font-bold text-sky-600">Our Tech Stack</h4>
-              <li className="text-gray-800 py-1">JavaScript & TypeScript</li>
-              <li className="text-gray-800 py-1">TailwindCSS</li>
-              <li className="text-gray-800 py-1">Svelte</li>
-              <li className="text-gray-800 py-1">Wordpress</li>
+              <h4 className="py-2 md:py-4 font-bold text-sky-600">Our Tech Stack</h4>
+              <ul className="px-4 list-disc text-sm md:text-base text-gray-800 dark:text-gray-200">
+                <li className="text-gray-800 py-1">JavaScript & TypeScript</li>
+                <li className="py-1">TailwindCSS</li>
+                <li className=" py-1">Svelte</li>
+                <li className="py-1">Wordpress</li>
+              </ul>
+
             </div>
-            <div className="text-left shadow-xl hover:shadow-2xl p-10 rounded-xl my-10  dark:bg-slate-200 flex-1">
+            <div className="text-left leading-8 shadow-xl hover:shadow-2xl p-4 md:p-10 rounded-xl my-4 md:my-10 dark:bg-slate-200">
               <Image src={code} alt="" width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 text-slate-600 ">
+              <h3 className="text-lg md:text-xl font-medium pt-4 md:pt-8 pb-2 text-slate-600 ">
                 Digital Marketing
               </h3>
-              <p className="py-2">
+              <p className="text-sm md:text-base py-2">
                 Elevate your digital presence with our strategic prowess, turning
                 clicks into conversions and engagement into lasting relationships.
               </p>
-              <h4 className="py-4 font-bold text-sky-600">Our Forte</h4>
-              <li className="text-gray-800 py-1">Digital Marketing Strategy</li>
-              <li className="text-gray-800 py-1">Social Media Management</li>
-              <li className="text-gray-800 py-1">Graphic Design</li>
-              <li className="text-gray-800 py-1">Photography & Videography</li>
+              <h4 className="py-2 md:py-4 font-bold text-sky-600">Our Forte</h4>
+              <ul className="px-4 list-disc text-sm md:text-base text-gray-800 dark:text-gray-200">
+                <li className="py-1">Digital Marketing Strategy</li>
+                <li className="py-1">Social Media Management</li>
+                <li className="py-1">Graphic Design</li>
+                <li className="py-1">Photography & Videography</li>
+              </ul>
             </div>
-            <div className="text-left shadow-xl hover:shadow-2xl p-10 rounded-xl my-10  dark:bg-slate-200 flex-1">
+            <div className="text-left shadow-xl leading-8 hover:shadow-2xl p-4 md:p-10 rounded-xl my-4 md:my-10 dark:bg-slate-200">
               <Image src={consulting} alt="" width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 text-slate-600 ">
+              <h3 className="text-lg md:text-xl font-medium pt-4 md:pt-8 pb-2 text-slate-600 ">
                 Software Solutions
               </h3>
-              <p className="py-2">
+              <p className="text-sm md:text-base py-2">
                 Experience the pinnacle of software development expertise and
                 bespoke solutions, to propel your business forward with precision.
               </p>
-              <h4 className="py-4 font-bold text-sky-600">Our Solutions</h4>
-              <li className="text-gray-800 py-1">Software Development</li>
-              <li className="text-gray-800 py-1">Mobile App development</li>
-              <li className="text-gray-800 py-1">ERP systems</li>
-              <li className="text-gray-800 py-1">CRM systems</li>
+              <h4 className="py-2 md:py-4 font-bold text-sky-600">Our Solutions</h4>
+              <ul className="px-4 list-disc text-sm md:text-base text-gray-800 dark:text-gray-200">
+                <li className="py-1">Software Development</li>
+                <li className="py-1">Mobile App development</li>
+                <li className="py-1">ERP systems</li>
+                <li className="py-1">CRM systems</li>
+              </ul>
             </div>
           </div>
         </section>
 
         {/**Portfolio section */}
-        <section className="py-10 px-20">
+        <section className="mt-10 px-4 md:px-6 lg:px-20">
           <div>
             <h3 className="text-3xl py-1 text-slate-700 dark:text-white font-bold">Portofolio</h3>
-            <p className="text-md py-2 leading-8  text-gray-800 dark:text-gray-200  mx-auto md:text-xl">
+            <p className="text-md py-2 leading-8  text-gray-800 dark:text-gray-200  mx-auto md:text-l">
               In every project, we dedicate ourselves wholeheartedly,
               harnessing our<span className="text-sky-600"> collective expertise and passion</span> to deliver exceptional
               solutions that redefine standards and exceed expectations,
@@ -175,7 +183,7 @@ export default function Home() {
 
           {/**Logos */}
 
-          <div className="grid grid-cols-6 gap-6 m-6 pt-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 m-4 md:m-6 pt-6">
             <div className="size-40">
               <a href="https://www.bbsltd.co.ke" target="_blank" rel="noopener noreferrer">
                 <Image
